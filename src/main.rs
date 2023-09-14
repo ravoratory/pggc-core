59261648e93473ad94a810960d812680b32935a7
yuji96 <y.yama683@gmail.com>
2023-09-14 15:15:29 +0900

    greeter を judger っぽくした

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,42 @@
-use tonic::{metadata::MetadataValue, transport::Server, Request, Response, Status};
 use dotenv::dotenv;
+use tonic::{metadata::MetadataValue, transport::Server, Request, Response, Status};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use judgement::judger_server::{Judger, JudgerServer};
+use judgement::{JudgeRequest, JudgeResponse};
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+pub mod judgement {
+    tonic::include_proto!("judgement");
 }
 
 #[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct MyJudger {}
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
+impl Judger for MyJudger {
+    async fn judge(
         &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request: {:?}", request);
+        request: Request<JudgeRequest>,
+    ) -> Result<Response<JudgeResponse>, Status> {
+        let team = &request.get_ref().team;
+        // let problem: &u8 = &request.get_ref().problem_id.parse().unwrap();
+        let problem = &request.get_ref().problem_name;
 
-        let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(),
+        dbg!(team, problem);
+
+        let response = judgement::JudgeResponse {
+            is_correct: "true".to_string(),
         };
 
-        Ok(Response::new(reply))
+        Ok(Response::new(response))
     }
 }
 
 fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
-    let token_string = std::env::var("VERIFY_TOKEN").expect("You should set variables 'VERIFY_TOKEN'");
-    let token: MetadataValue<_> = format!("Bearer {token_string}", token_string=token_string).parse().unwrap();
+    let token_string =
+        std::env::var("VERIFY_TOKEN").expect("You should set variables 'VERIFY_TOKEN'");
+    let token: MetadataValue<_> = format!("Bearer {token_string}", token_string = token_string)
+        .parse()
+        .unwrap();
 
     match req.metadata().get("authorization") {
         Some(t) if token == t => Ok(req),
@@ -40,11 +47,11 @@ fn check_auth(req: Request<()>) -> Result<Request<()>, Status> {
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:50051".parse()?;
-    let server = MyGreeter::default();
+    let server = MyJudger::default();
     dotenv().ok();
 
     Server::builder()
-        .add_service(GreeterServer::with_interceptor(server, check_auth))
+        .add_service(JudgerServer::with_interceptor(server, check_auth))
         .serve(addr)
         .await?;
 
